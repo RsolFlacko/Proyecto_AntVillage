52baee8d751193e170d2db6cb3286e848611b46f
Roger Solano "Pretty Flacko <81654531+RsolFlacko@users.noreply.github.com>
2022-06-16 21:13:44 -0600

    Actualización de algunas clases

---
+++ Proyecto3_AntVillage/src/proyecto3_antvillage/Nodo.java
@@ -0,0 +1,62 @@
+
+package proyecto3_antvillage;
+
+/**
+ *
+ * @author pc
+ */
+public class Nodo {
+    private int X;
+    private int Y;
+    public Nodo next;
+    private Nodo tail;
+    private int tipoposiciones;
+    
+    public Nodo() {
+        this.X = 0;
+        this.Y = 0;
+        this.next = null;
+        this.tail = null;
+        this.tipoposiciones=0;
+    }
+    
+    public void setcoordenadas(int ejeX,int ejeY) {
+        this.X = ejeX;
+        this.Y = ejeY;
+    }
+    /**
+     *
+     * @param tipo
+     */
+    public void setTipoposiciones(int tipo) {
+        this.tipoposiciones=tipo;
+    }
+    
+    public int getTipoposiciones(){
+        return this.tipoposiciones;
+    }
+    
+    public int getX() {
+            return this.X;
+        }
+    
+    public int getY() {
+            return this.Y;
+        }
+    
+    public void setNext(Nodo Node) {
+            this.next = Node;
+        }
+    
+    public Nodo getNext() {
+            return this.next;
+        }
+    
+    public void setTail(Nodo Node) {
+        this.tail = Node;
+    }
+    
+    public Nodo getTail() {
+        return this.tail;
+    }
+}
